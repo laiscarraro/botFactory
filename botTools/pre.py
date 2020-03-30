28022e3e925dbefdc8bb78d7ef557ac732c8b90f
laiscarraro <laiscarraro@usp.br>
2020-03-30 20:05:54 +0000

    separei pré processamento em opções

--- botTools/pre.py
+++ botTools/pre.py
@@ -1,14 +1,41 @@
-!python -m spacy download pt
+import subprocess
 import spacy
 import nltk
 
-nltk.download('stopwords')
+try:
+  from nltk.corpus import stopwords
+except:
+  nltk.download('stopwords')
+  from nltk.corpus import stopwords
 
-from nltk.corpus import stopwords
+'''
+Essa é a função mágica do pré-processamento de texto!!!
+Nela, você pode escolher incluir:
 
-def pre_processamento(texto):
-  nlp = spacy.load('pt')
+1 - Tokenização por palavra: tokenizar significa separar; tokenizar por palavra, basicamente significa retornar um array com as palavras do texto.
+2 - Remoção de stopwords: stopwords são aquelas palavras que só fazem volume no texto, e não têm muito significado. Tirar stopwords requer tokenização que, portanto, está implícita neste passo.
+3 - Tagging de classes gramaticais: encontra a classe gramatical de cada palavra do texto, e requer tokenização (implícito).
+4 - Lemmatização: basicamente passa os verbos para o infinitivo. Palavras como "mim" e "me" também contam como a mesma coisa. Requer tagging de classe gramatical e idealmente remoção de stopwords.
+'''
+
+def pre_processamento(texto, so_tokenizar=False, sem_stopwords=True, lemma=True):
+  try:
+    nlp = spacy.load('pt')
+  except:
+    subprocess.call('python -m spacy download pt', shell=True)
+    nlp = spacy.load('pt')
   texto2 = nlp(u''+texto)
-  lemmatizado = [token.lemma_ for token in texto2]
-  lixo = set(stopwords.words('portuguese'))
-  return [token for token in lemmatizado if token not in lixo]
+  resp = texto2
+  if so_tokenizar:
+    return texto2.text.split()
+  if lemma:
+    lemmatizado = [token.lemma_ for token in texto2]
+    resp = lemmatizado
+  if sem_stopwords:
+    try:
+      lixo = set(stopwords.words('portuguese'))
+    except:
+      
+    no_stop = [token for token in resp if token not in lixo]
+    resp = no_stop
+  return resp
